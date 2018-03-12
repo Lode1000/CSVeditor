0224329431ca678945d5e69e902fee36b1e7d8ad
Lode1000 <lode1000@gmail.com>
2018-03-12 11:22:37 +0200

    final

--- src/tui/TiedonKasittely.java
+++ src/tui/TiedonKasittely.java
@@ -81,7 +81,7 @@ public class TiedonKasittely implements Kayttoliittyma{
     }
 
     private void suoritaKaikkiToimenpiteet(){
-        tiedonKasittelija.jataMerkkienJalkeiset(tietosisalto.getSarakkeet(),2,"-", false);
+        tiedonKasittelija.jataMerkkienJalkeiset(tietosisalto.getSarakkeet(),2,"-", true);
         // tiedonKasittelija.jataViimeisetSanat(tietosisalto.getSarakkeet(),2, 2);
         tiedonKasittelija.jarjestaAakkosittain(tietosisalto, 2,true);
         tiedonKasittelija.poistaDuplikaatit(tietosisalto.getSarakkeet(),2);
